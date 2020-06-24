9c4a0ecd06f6ebd2b76fb3ecddd472711828b117
Gshyx <1970312749@qq.com>
2020-06-24 20:11:43 +0800

    first page commit

---
+++ cloudfunctions/query_anygoods/index.js
@@ -0,0 +1,24 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init()
+const db = cloud.database()
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  const wxContext = cloud.getWXContext()
+
+  return await db.collection('goods')
+  .get({
+    success: function(res) {
+      console(res.data)
+    }
+  })
+
+  return {
+    event,
+    openid: wxContext.OPENID,
+    appid: wxContext.APPID,
+    unionid: wxContext.UNIONID,
+  }
+}
